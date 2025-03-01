eabb9bb90c841478f7ebda852f0cbd998ac8d2dc
gauthamijanjirala <gauthamijanjirala14@gmail.com>
2025-03-01 08:25:44 +0530

    test

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -1932,7 +1932,7 @@ Constrain images and videos to the parent width and preserve their intrinsic asp
                                             d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z">
                                         </path>
                                     </svg></div>
-                                <h3 class="text-xl font-bold text-gray-900 mb-3">3. Secure Payment</h3>
+                                <h3 class="text-xl font-bold text-gray-900 mb-3">3. payment</h3>
                                 <p class="text-gray-600">Funds are held in escrow until you approve the work, ensuring
                                     security for both parties.</p>
                             </div>
